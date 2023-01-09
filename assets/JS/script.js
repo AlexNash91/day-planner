9fb2f53bb06de41b29654aa3ba81124dd167e279
Alex Nash <Alexr.nash@gmail.com>
2023-01-09 12:01:22 -0500

    got text form working

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,11 +7,12 @@ $(function () {
         }
     
       setInterval(update, 1000);
-
+        // todo set a getitem method before the clicker
+        
         // Event listener blcok, code not passing data into local storage, will fix later
         $("button").click(function() {
             console.log("test")
-            var event = $("textarea")
+            var event = $("textarea").val()
             localStorage.setItem("Event", JSON.stringify(event))
         
     })
