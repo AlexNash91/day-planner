8445554de335eb583c8f4e64f72a0a7a3c194f93
Alex Nash <Alexr.nash@gmail.com>
2023-01-11 16:04:56 -0500

    cleaned up comments and console logs

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    // Date block, still need to figure out day of week
+    
     function update() {
-        // var isoWeek = require('dayjs/plugin/isoWeek')
+        
         let pDate = dayjs().format('dddd, MMMM DD');
         $('#currentDay').text(pDate);
         }
@@ -9,9 +9,9 @@ $(document).ready(function () {
         const time = dayjs().hour()
         console.log(time)
 
-
-
       setInterval(update, 1000);
+
+
         // event loop
         const hourArr = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13","hour-14","hour-15","hour-16","hour-17" ]
         for (i=0; i < hourArr.length; i++) {
@@ -35,22 +35,13 @@ $(document).ready(function () {
 
 
         }
-
-        
-
-        // todo set a getitem method before the clicker 
-        // figure out arguments for getItem
-        
-        // console.log(hour)
+         
         
         // Event listener block, code passing into all blocks 
         $("button").click(function() {
-          // console.log(typeof(hour))
-          const hour = $(this).parent().attr('id')
-          // console.log("test")
+          const hour = $(this).parent().attr('id')          
           var event = $("textarea").val()
-          localStorage.setItem(hour, JSON.stringify(event))
-          
-          
+          localStorage.setItem(hour, JSON.stringify(event))         
         })
+
       })
